64fb99d0c65e22e1ea4f6a469f9cdc3d785f5d2b
Jochen Hunz <j.hunz@anchorpoint.app>
2021-12-03 17:32:45 +0100

    store settings per folder in new_folder_from_template so that creating a new folder from a template becomes a much nicer action

--- template/new_folder_from_template.py
+++ template/new_folder_from_template.py
@@ -17,8 +17,10 @@ dialog_name_var = "name"
 resolved_vars = []
 
 import sys
+
 sys.path.insert(0, ctx.yaml_dir)
 import template_utility
+
 
 def map_variable_type(variable_type_str):
     if variable_type_str == "Name":
@@ -31,11 +33,13 @@ def map_variable_type(variable_type_str):
         return template_utility.VariableType.INCREMENT
     return template_utility.VariableType.NAME
 
-def resolve_variables(path, resolved_vars):   
+
+def resolve_variables(path, resolved_vars):
     for var in resolved_vars:
         path = path.replace("$", var, 1)
-    
+
     return path
+
 
 def resolve_dialog_variables(dialog, template):
     variable = 0
@@ -46,46 +50,80 @@ def resolve_dialog_variables(dialog, template):
         if "$" not in template:
             break
 
-        type = dialog.get_value(dialog_type_var+str(variable))
-        var_name = dialog.get_value(dialog_name_var+str(variable))
-        resolved_var = template_utility.resolve_variable(template, map_variable_type(type), folder, var_name, username)
+        type = dialog.get_value(dialog_type_var + str(variable))
+        var_name = dialog.get_value(dialog_name_var + str(variable))
+        resolved_var = template_utility.resolve_variable(
+            template, map_variable_type(type), folder, var_name, username
+        )
         template = template.replace("$", resolved_var, 1)
         variable = variable + 1
         resolved_vars.append(resolved_var)
 
     return template
 
+
 def update_preview(dialog):
     template = dialog.get_value(dialog_template_var)
     template = resolve_dialog_variables(dialog, template)
     dialog.set_value(dialog_preview_var, template)
-    
+
+
 def dropdown_changed(variable, dialog, value):
-    name_entry = dialog.get(dialog_name_var+str(variable))
+    name_entry = dialog.get(dialog_name_var + str(variable))
     if name_entry:
         name_entry.enabled = value == "Name"
     update_preview(dialog)
 
+
 def name_changed(dialog, value):
     update_preview(dialog)
+
 
 def rename_folder_entries(dialog, target):
     for root, _, files in os.walk(target):
         if root != target:
             resolved = resolve_variables(root, resolved_vars)
-            if resolved != root: 
+            if resolved != root:
                 aps.rename_folder(api, root, resolved)
 
         for file in files:
             resolved = resolve_variables(file, resolved_vars)
-            if resolved != file: 
-                aps.rename_file(api, os.path.join(root, file), os.path.join(root, resolved))
+            if resolved != file:
+                aps.rename_file(
+                    api, os.path.join(root, file), os.path.join(root, resolved)
+                )
+
+
+def store_settings(dialog):
+    settings = aps.Settings(api)
+    variable = 0
+    while True:
+        type = dialog.get_value(dialog_type_var + str(variable))
+        var_name = dialog.get_value(dialog_name_var + str(variable))
+
+        print(type)
+        print(var_name)
+
+        if type == None and var_name == None:
+            break
+
+        if type:
+            settings.set(f"{folder}_{dialog_type_var}{variable}", type)
+
+        if var_name:
+            settings.set(f"{folder}_{dialog_name_var}{variable}", var_name)
+
+        variable = variable + 1
+
+    settings.store()
+
 
 def button_pressed(dialog, source):
     preview = dialog.get_value(dialog_preview_var)
 
-    def copy_folder_async():    
+    def copy_folder_async():
         try:
+            store_settings(dialog)
             target = os.path.join(folder, preview)
             aps.copy_folder(api, source, target)
             rename_folder_entries(dialog, target)
@@ -94,11 +132,14 @@ def button_pressed(dialog, source):
             ui.show_error("Could not create Folder", str(e))
         else:
             ui.show_success("folder copied")
-    
+
     ctx.run_async(copy_folder_async)
     dialog.close()
 
-def create_copy_dialog(variable_count, template_folders, target_folder):
+
+def create_copy_dialog(variable_count, template_folders):
+    settings = aps.Settings(api)
+
     dialog = ap.Dialog()
     dialog.title = "New Folder from Template"
     if ctx.icon:
@@ -106,26 +147,48 @@ def create_copy_dialog(variable_count, template_folders, target_folder):
     if ctx.icon_color:
         dialog.icon_color = ctx.icon_color
 
-    dialog.add_text("Template:\t").add_input(os.path.basename(template_folders), enabled=False, var=dialog_template_var)
-    dialog.add_text("Preview:\t").add_input("Test", enabled=False, var=dialog_preview_var)
+    dialog.add_text("Template:\t").add_input(
+        os.path.basename(template_folders), enabled=False, var=dialog_template_var
+    )
+    dialog.add_text("Preview:\t").add_input(
+        "Test", enabled=False, var=dialog_preview_var
+    )
     dialog.add_separator()
 
     for variable in range(variable_count):
+        type = settings.get(
+            f"{folder}_{dialog_type_var}{variable}",
+            "Date of Today" if variable == 0 else "Name",
+        )
+        input = settings.get(f"{folder}_{dialog_name_var}{variable}", "")
+
         dialog.start_section(f"variable {variable+1}", foldable=False)
-        
-        default = "Date of Today" if variable == 0 else "Name"
-        dialog.add_dropdown(default, ["Increment", "Date of Today", "User Initials", "Name"], var=dialog_type_var+str(variable), \
-            callback=lambda d,v,var=variable : dropdown_changed(var,d,v))
-        dialog.add_input("", enabled=default=="Name", var=dialog_name_var+str(variable), callback=name_changed)
+
+        dialog.add_dropdown(
+            type,
+            ["Increment", "Date of Today", "User Initials", "Name"],
+            var=dialog_type_var + str(variable),
+            callback=lambda d, v, var=variable: dropdown_changed(var, d, v),
+        )
+        dialog.add_input(
+            input,
+            enabled=type == "Name",
+            var=dialog_name_var + str(variable),
+            callback=name_changed,
+        )
         dialog.end_section()
 
-    dialog.add_button("Create Folder", callback=lambda d,src=template_folders: button_pressed(d,src))
+    dialog.add_button(
+        "Create Folder", callback=lambda d, src=template_folders: button_pressed(d, src)
+    )
 
     update_preview(dialog)
     return dialog
 
+
 def get_variables_count(template_folders):
     return template_folders.count("$")
+
 
 def check_folder_conflict(folder):
     if os.path.exists(folder):
@@ -133,23 +196,26 @@ def check_folder_conflict(folder):
         return True
     return False
 
+
 def copy_folder_no_variables(template_folders, target_folder):
     if check_folder_conflict(target_folder):
-        return    
-    
+        return
+
     def copy_folder_no_variables_async():
         try:
             aps.copy_folder(api, template_folders, target_folder)
             template_utility.remove_gitkeep(target_folder)
         except Exception as e:
             ui.show_error("could not copy folder", str(e))
-    
+
     ctx.run_async(copy_folder_no_variables_async)
 
+
 def copy_folder_with_variables(variable_count, template_folders, target_folder):
-    dialog = create_copy_dialog(variable_count, template_folders, target_folder)
+    dialog = create_copy_dialog(variable_count, template_folders)
     if dialog:
         dialog.show()
+
 
 def copy_folder(template_folder, target_folder):
     if not os.path.exists(template_folder):
@@ -160,9 +226,9 @@ def copy_folder(template_folder, target_folder):
     if variable_count == 0:
         copy_folder_no_variables(template_folder, target_folder)
         return
-        
+
     copy_folder_with_variables(variable_count, template_folder, target_folder)
-    
+
 
 if "template_folder" not in ctx.inputs or len(ctx.inputs["template_folder"]) is 0:
     ui.show_error("template_folders not set", f"Please adapt {ctx.yaml}", duration=6000)
@@ -170,5 +236,7 @@ else:
     template_folder = ctx.inputs["template_folder"]
     if not os.path.exists(template_folder):
         template_folder = os.path.join(ctx.yaml_dir, template_folder)
-    
-    copy_folder(template_folder, os.path.join(ctx.path, os.path.basename(template_folder)))
+
+    copy_folder(
+        template_folder, os.path.join(folder, os.path.basename(template_folder))
+    )
