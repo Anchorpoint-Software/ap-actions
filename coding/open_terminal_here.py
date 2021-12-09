4b42375d1b063a8fc2ec1dfd4908d81827ab6cdf
Jochen Hunz <j.hunz@anchorpoint.app>
2021-12-09 16:01:06 +0100

    fix for open terminal here action for paths with spaces

--- coding/open_terminal_here.py
+++ coding/open_terminal_here.py
@@ -3,8 +3,7 @@ import os
 import platform
 
 ctx = ap.Context.instance()
-
 if platform.system() == "Darwin":
-    os.system(f"open -a Terminal {ctx.path}")
+    os.system(f"open -a Terminal \"{ctx.path}\"")
 elif platform.system() == "Windows":
-    os.system(f"start cmd /k cd {ctx.path}")
+    os.system(f"start cmd /k cd \"{ctx.path}\"")
