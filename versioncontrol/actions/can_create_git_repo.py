ebe9cda86e452a110399d6b3aa040a8fd627c55e
Jochen Hunz <j.hunz@anchorpoint.app>
2022-05-09 17:04:35 +0200

    implement git get pending changes hook

--- versioncontrol/actions/can_create_git_repo.py
+++ versioncontrol/actions/can_create_git_repo.py
@@ -2,8 +2,12 @@ import anchorpoint as ap
 from pathlib import Path
 import os, sys
 
-sys.path.insert(0, os.path.split(__file__)[0])
-import is_git_repo as git
+try:
 
-def on_action_enable(path: str, ctx: ap.Context) -> bool:
+    sys.path.insert(0, os.path.split(__file__)[0])
+    import is_git_repo as git
+except Exception as e:
+    print(str(e))
+
+def on_is_action_enabled(path: str, ctx: ap.Context) -> bool:
     return git.is_git_repo(path)
--- versioncontrol/actions/git_get_pending_changes.py
+++ versioncontrol/actions/git_get_pending_changes.py
@@ -1,43 +1,84 @@
+from dataclasses import dataclass
 import anchorpoint as ap
-import apsync as aps
+from enum import Enum
+from typing import Optional
 
-import sys, os, importlib
-sys.path.insert(0, os.path.join(os.path.split(__file__)[0], ".."))
+class Status(Enum):
+    NEW = 1
+    DELETED = 2
+    MODIFIED = 3
+    RENAMED = 4
+    CONFLICTED = 5
 
-importlib.invalidate_caches()
-from vc.apgit.repository import * 
+@dataclass
+class PendingChange:
+    status: Status
+    path: str
+    old_path: Optional[str] = None
+    preview: Optional[str] = None
 
-ctx = ap.Context.instance()
-ui = ap.UI()
-path = ctx.path
+    def __eq__(self, __o: object) -> bool:
+        return self.path == __o.path
 
-def print_changes():
+    def __ne__(self, __o: object) -> bool:
+        return self.path != object.path
+
+    def __hash__(self) -> int:
+        return self.path.__hash__()
+
+	
+def parse_change(change, status: Status) -> PendingChange:
+    result = PendingChange(path = change.path, old_path = change.old_path, status = status)
+    return result
+
+def parse_changes(repo_changes, changes: set[PendingChange]):
+    for file in repo_changes.new_files:
+        changes.add(parse_change(file, Status.NEW))
+    for file in repo_changes.modified_files:
+        changes.add(parse_change(file, Status.MODIFIED))
+    for file in repo_changes.deleted_files:
+        changes.add(parse_change(file, Status.DELETED))
+    for file in repo_changes.renamed_files:
+        changes.add(parse_change(file, Status.RENAMED))
+
+def parse_conflicts(conflicts, changes: set[PendingChange]):
+    for conflict in conflicts:
+        if conflict in changes:
+            changes[conflict].status = Status.CONFLICTED
+
+def on_vc_get_pending_changes(path: str, ctx: ap.Context):
+    import sys, os
+    sys.path.insert(0, os.path.split(__file__)[0])
+    import is_git_repo as git
+
+    if not git.path_contains_git_repo(path):
+        return None
+    
+    sys.path.insert(0, os.path.join(os.path.split(__file__)[0], ".."))
+    from vc.apgit.repository import GitRepository
+    
     repo = GitRepository.load(path)
     if repo == None: return
 
-    ui.clear_console()
-    
-    conflicts = repo.get_conflicts()
-    if len(conflicts) > 0:
-        if repo.is_rebasing():
-            print("***Rebasing***\nResolve conflicts or cancel merge")
-            print("==========================\n\n")
+    changes = set[PendingChange]()
 
-        print("Conflicts")
-        print("=========")
-        for conflict in conflicts:
-            print(conflict)
-    else:    
-        print("Uncommitted Changes")
-        print("==================")
-        uncommitted = repo.get_pending_changes(staged = False)
-        print(uncommitted)
+    try:
+        parse_changes(repo.get_pending_changes(staged = True), changes)
+        parse_changes(repo.get_pending_changes(staged = False), changes)
+        parse_conflicts(repo.get_conflicts(), changes)
+    except ValueError:
+        # Catches errors such as unborn repositories
+        return changes
 
-        print("\n\nStaged Changes")
-        print("==============")
-        staged = repo.get_pending_changes(staged = True)
-        print(staged)
+    return list(changes)
 
-    ui.show_console()
+if __name__ == "__main__":
+    ctx = ap.Context.instance()
+    path = ctx.path
 
-ctx.run_async(print_changes)
+    changes = on_vc_get_pending_changes(path, ctx)
+    if not changes:
+        ap.UI().show_info("Not a Git repository")
+    else:
+        for change in changes:
+            print (change.path, "  |  " , change.status)
