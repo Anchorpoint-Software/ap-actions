898139663ce8261f57b5500f4f9813748a76b03e
Jochen Hunz <j.hunz@anchorpoint.app>
2022-05-16 13:55:55 +0200

    updated hook examples

--- examples/trigger/attributes_changed_example.py
+++ examples/trigger/attributes_changed_example.py
@@ -4,4 +4,4 @@ def on_attributes_changed(parent_path: str, attributes: list[ap.AttributeChange]
     ap.UI().show_console()
     print(f"{len(attributes)} Attribute(s) Changed in: {[parent_path]}")
     for change in attributes:
-        print(f"Attribute Changed Python Trigger:  {change.path} {change.name}: {change.value}")
+        print(f"Attribute Changed Python Trigger:  {change.path} {change.name}: {change.old_value} -> {change.value}")
