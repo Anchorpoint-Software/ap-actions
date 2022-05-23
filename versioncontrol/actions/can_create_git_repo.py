9f2f92508e8e1fe5855a2d9360a4ca26109bd067
Jochen Hunz <j.hunz@anchorpoint.app>
2022-05-23 16:32:39 +0200

    adapt can create repo hook

--- versioncontrol/actions/can_create_git_repo.py
+++ versioncontrol/actions/can_create_git_repo.py
@@ -3,11 +3,10 @@ from pathlib import Path
 import os, sys
 
 try:
-
     sys.path.insert(0, os.path.split(__file__)[0])
     import is_git_repo as git
 except Exception as e:
     print(str(e))
 
-def on_is_action_enabled(path: str, ctx: ap.Context) -> bool:
+def on_is_action_enabled(path: str, type: ap.Type, ctx: ap.Context) -> bool:
     return git.is_git_repo(path)
