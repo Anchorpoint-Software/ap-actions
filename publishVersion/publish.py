f2bd1e62236e6a2ba7c122224c3163d94df1bd8c
Matthäus <m.niedoba@anchorpoint.app>
2022-05-26 20:08:42 +0200

    Publish Version Action
    This is an experiment to learn more about publishing workflows

---
+++ publishVersion/publish.py
@@ -0,0 +1,59 @@
+import anchorpoint as ap
+import apsync as aps
+import os
+
+ctx = ap.Context.instance()
+project = aps.get_project(ctx.project_path)
+ui = ap.UI()
+settings = project.get_metadata()
+
+def containsNumber(value):
+    for character in value:
+        if character.isdigit():
+            return True
+    return False
+
+def copy():
+    splitted_name = ctx.filename.split("_")
+    lastSplitPart = splitted_name[len(splitted_name)-1]
+    if(containsNumber(lastSplitPart)):
+        progress = ap.Progress("Publishing", "Creating a copy")
+        splitted_name.pop()
+
+        new_name = ""
+        for i in splitted_name:
+            new_name+=i
+            new_name+="_"
+        new_name = new_name[:-1]
+        
+        new_name_appendix = new_name
+
+        try:
+            new_name_appendix+=settings["publish_version_appendix"]
+        except:
+            pass
+
+        new_location = ctx.folder
+
+        try:
+            if(settings["publish_file_location"] != ""):
+                new_location=settings["publish_file_location"]
+        except:
+            new_location = ctx.folder
+
+        
+
+        new_path = os.path.join(new_location,new_name_appendix+"."+ctx.suffix)
+
+        aps.copy_file(ctx.path,new_path,overwrite=True)
+        aps.set_attribute_text(new_path,"Source File",ctx.filename)
+        
+        ui.show_success(f"Published {new_name_appendix}",f"Published in {new_location}")
+        progress.finish()
+
+    else:
+        ui.show_error("Not an increment","This file has no v001 or similar")
+        return
+    
+
+ctx.run_async(copy) 
