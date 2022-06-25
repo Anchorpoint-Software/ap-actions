fa26c4724ae46645b5810d3d304ddc69c25de6f6
Jochen Hunz <j.hunz@anchorpoint.app>
2022-06-25 18:32:19 +0200

    remove trigger examples for now as they need polishment

--- examples/trigger/on_timeout.py
+++
@@ -1,5 +0,0 @@
-import anchorpoint as ap
-
-def on_timeout(ctx: ap.Context):
-    print(f"action called from time trigger {ctx}")
-    print(f"Selected: {ctx.selected_folders} {ctx.selected_files}")
