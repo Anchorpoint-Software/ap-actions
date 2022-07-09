5eb279650c708ee255b7100569b62012e46afb2c
Jochen Hunz <j.hunz@anchorpoint.app>
2022-07-09 20:17:40 +0200

    fix cancellation of lfs was not working

--- versioncontrol/vc/apgit/lfs.py
+++ versioncontrol/vc/apgit/lfs.py
@@ -1,5 +1,5 @@
 from git import RemoteProgress
-from vc.apgit.utility import get_git_cmd_path
+from vc.apgit.utility import get_lfs_path
 import subprocess, platform
 
 def _run_lfs_command(path: str, args, progress: RemoteProgress, env):
@@ -35,9 +35,9 @@ def _run_lfs_command(path: str, args, progress: RemoteProgress, env):
         raise RuntimeError(process.stderr)
 
 def lfs_fetch(path: str, remote: str, progress: RemoteProgress, env):
-    args = [get_git_cmd_path(), "lfs", "fetch", remote, "@{u}"]
+    args = [get_lfs_path(), "fetch", remote, "@{u}"]
     _run_lfs_command(path, args, progress, env)
 
 def lfs_push(path: str, remote: str, branch: str, progress: RemoteProgress, env):
-    args = [get_git_cmd_path(), "lfs", "push", remote, branch]
+    args = [get_lfs_path(), "push", remote, branch]
     _run_lfs_command(path, args, progress, env)
