62f32882aeda8c31e4b55bde359a9010637e57a7
Jochen Hunz <j.hunz@anchorpoint.app>
2022-07-21 09:58:15 +0200

    fix missing import

--- versioncontrol/actions/git_repository_helper.py
+++ versioncontrol/actions/git_repository_helper.py
@@ -2,6 +2,10 @@ import anchorpoint as ap
 import apsync as aps
 from typing import Optional
 
+import sys, os, importlib
+sys.path.insert(0, os.path.join(os.path.split(__file__)[0], ".."))
+import vc.versioncontrol_interface as vc
+
 CHANNEL_ID = "Git"
 
 def update_project(
