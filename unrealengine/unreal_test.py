7fcaa3e415472cd2f385f3c3e4ebbb514af540fd
William Hong <william_hong@hotmail.de>
2022-09-08 09:42:22 +0200

    wip: one actor per file

---
+++ unrealengine/unreal_test.py
@@ -0,0 +1,41 @@
+import unreal
+
+# captures a high res screenshot
+class capture(object):
+    def __init__(self):
+        unreal.EditorLevelLibrary.editor_set_game_view(True)
+        self.actors = (actor for actor in unreal.EditorLevelLibrary.get_selected_level_actors())
+        shot_name = "test"
+        unreal.AutomationLibrary.take_high_res_screenshot(1920,1080, shot_name + ".png")
+        
+# returns an array of the selected assets
+def get_selected_assets():
+    utility_base = unreal.GlobalEditorUtilityBase.get_default_object()
+    return utility_base.get_selected_assets()
+
+def get_asset_infos():
+    selectedAssets = get_selected_assets()
+    
+    for asset in selectedAssets:
+        print(asset.get_full_name())
+        print(asset.get_fname())
+        print(asset.get_path_name())
+
+#get_asset_infos()
+#capture()
+
+def test():
+    print(unreal.EditorLevelLibrary.get_editor_world())
+    actors = unreal.GameplayStatics.get_all_actors_of_class(unreal.EditorLevelLibrary.get_editor_world(), unreal.StaticMeshActor) # change this later to all actors? unreal.Actor
+    #for x in actors:
+     #   print(x)
+     
+    actor = actors[3]
+    print(actor.get_full_name())
+    print(actor.get_fname())
+    print(actor.get_path_name())
+    print(actor.get_folder_path())
+    print("folder path: ", actor.get_folder_path())
+     
+    
+test()
