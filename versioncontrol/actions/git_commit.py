7b100b2bd983cdcbb01c9cda927e0b8b49690c7f
Jochen Hunz <j.hunz@anchorpoint.app>
2022-09-20 20:15:58 +0200

    fix git_lfs_helper unknown

--- versioncontrol/actions/git_commit.py
+++ versioncontrol/actions/git_commit.py
@@ -10,8 +10,7 @@ from vc.apgit.repository import *
 from vc.apgit.utility import get_repo_path
 sys.path.remove(parent_dir)
 
-def stage_files(changes, repo):
-    import git_lfs_helper as lfs
+def stage_files(changes, repo, lfs):
     to_stage = []
     for change in changes:
         if change.selected:
@@ -20,14 +19,14 @@ def stage_files(changes, repo):
     lfs.lfs_track_binary_files(to_stage, repo)
     repo.sync_staged_files(to_stage)
 
-def commit_async(message: str, changes, channel_id, project_path):
+def commit_async(message: str, changes, channel_id, project_path, lfs):
     ui = ap.UI()
     progress = ap.Progress("Committing Files", "Depending on your file count and size this may take some time", show_loading_screen=True)
     try:
         path = get_repo_path(channel_id, project_path)
         repo = GitRepository.load(path)
         if not repo: return
-        stage_files(changes, repo)
+        stage_files(changes, repo, lfs)
 
         staged = repo.get_pending_changes(staged=True)
         changecount = staged.size()
@@ -42,6 +41,7 @@ def commit_async(message: str, changes, channel_id, project_path):
         ui.show_error("Commit Failed", str(e))
 
 def on_pending_changes_action(channel_id: str, action_id: str, message: str, changes, ctx):
+    import git_lfs_helper as lfs
     if action_id != "gitcommit": return False
-    ctx.run_async(commit_async, message, changes, channel_id, ctx.project_path)
+    ctx.run_async(commit_async, message, changes, channel_id, ctx.project_path, lfs)
     return True
