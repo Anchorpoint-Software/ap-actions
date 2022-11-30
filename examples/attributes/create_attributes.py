9a45acbcde6d0be7c278a0ef59920410299c1ff3
Jochen Hunz <j.hunz@anchorpoint.app>
2022-11-30 21:02:55 +0100

    change attribute example to work in different workspace

--- examples/attributes/create_attributes.py
+++ examples/attributes/create_attributes.py
@@ -9,14 +9,14 @@ selected_files = ctx.selected_files
 selected_folders = ctx.selected_folders
 
 def create_attribute(object):
-    aps.set_attribute_text(object, "Text", "Hello")
-    aps.set_attribute_link(object, "Link", "https://www.anchorpoint.app")
-    aps.set_attribute_checked(object, "Checkbox", True)
-    aps.set_attribute_date(object, "Date", int(time.time()))
-    aps.set_attribute_rating(object, "Rating", 4)
-    aps.set_attribute_tag(object, "Single Choice Tag", "Python <3", tag_color=aps.TagColor.green)
-    aps.set_attribute_tag(object, "Multiple Choice Tag 1", "Anchorpoint", tag_color=aps.TagColor.blue, type=aps.AttributeType.multiple_choice_tag)
-    aps.set_attribute_tags(object, "Multiple Choice Tag 2", ["JPEG","PNG"])
+    aps.set_attribute_text(object, "Text", "Hello", workspace_id = ctx.workspace_id)
+    aps.set_attribute_link(object, "Link", "https://www.anchorpoint.app", workspace_id = ctx.workspace_id)
+    aps.set_attribute_checked(object, "Checkbox", True, workspace_id = ctx.workspace_id)
+    aps.set_attribute_date(object, "Date", int(time.time()), workspace_id = ctx.workspace_id)
+    aps.set_attribute_rating(object, "Rating", 4, workspace_id = ctx.workspace_id)
+    aps.set_attribute_tag(object, "Single Choice Tag", "Python <3", tag_color=aps.TagColor.green, workspace_id = ctx.workspace_id)
+    aps.set_attribute_tag(object, "Multiple Choice Tag 1", "Anchorpoint", tag_color=aps.TagColor.blue, type=aps.AttributeType.multiple_choice_tag, workspace_id = ctx.workspace_id)
+    aps.set_attribute_tags(object, "Multiple Choice Tag 2", ["JPEG","PNG"], workspace_id = ctx.workspace_id)
 
     print(aps.get_attribute_text(object, "Text"))
     print(aps.get_attribute_tag(object, "Single Choice Tag").name)
