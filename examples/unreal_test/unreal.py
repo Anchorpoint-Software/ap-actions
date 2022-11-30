623349f5b0427e9bf6e00bc2fe70aaebeac1ceef
William Hong <william_hong@hotmail.de>
2022-11-30 10:53:44 +0100

    improved thumbnail path handling

--- examples/unreal_test/unreal.py
+++ examples/unreal_test/unreal.py
@@ -10,13 +10,18 @@ ctx = ap.Context.instance()
 
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
 
+# use filename as thumbnail output name
+path = ctx.path
+temp = path.split("/")[-1]
+file_name = temp.split(".")[0]
+
 msg = aps.IpcMessage()
 msg.topic = "APUnreal"
 msg.kind = "hello"
 msg.header = {
     "task": "Thumbnail",
-    "path": ctx.path,
-    "output_path": os.path.join(desktop, "test.png")
+    "path": path,
+    "output_path": os.path.join(desktop, file_name+".png")
     }
 
 print(ctx.path)
