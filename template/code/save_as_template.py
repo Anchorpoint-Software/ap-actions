50f9e597b16b0a6a0582b0ed4219d88f35db8a0d
Jochen Hunz <j.hunz@anchorpoint.app>
2022-12-08 11:19:29 +0100

    fix template action does not work with old client

--- template/code/save_as_template.py
+++ template/code/save_as_template.py
@@ -15,7 +15,11 @@ source = ctx.path
 settings = aps.SharedSettings(ctx.workspace_id, "AnchorpointTemplateSettings")
 template_dir = settings.get("template_dir", template_dir)
 callback_file = os.path.join(settings.get("callback_dir"), "template_action_events.py")
-workspace_id = ctx.workspace_id
+
+if aps.get_api_version() >= aps.ApiVersion("1.2.0"):
+    workspace_id_kwarg = {"workspace_id": ctx.workspace_id}
+else:
+    workspace_id_kwarg = {}
 
 project = aps.get_project(source)
 if project:
@@ -48,12 +52,12 @@ def create_template_async(name, source, target):
                 return
                 
             os.makedirs(target)
-            aps.copy_folder(source, target, workspace_id=workspace_id)
+            aps.copy_folder(source, target, **workspace_id_kwarg)
             if callbacks and "folder_template_saved" in dir(callbacks):
                 callbacks.folder_template_saved(name, target)
         else:
             os.makedirs(os.path.dirname(target))
-            aps.copy_file(source, target, workspace_id=workspace_id)
+            aps.copy_file(source, target, **workspace_id_kwarg)
             if callbacks and "file_template_saved" in dir(callbacks):
                 callbacks.file_template_saved(name, target)
 
--- template/code/templates.py
+++ template/code/templates.py
@@ -40,7 +40,11 @@ template = ctx.inputs["template_dir"]
 template_subdir = ctx.inputs["template_subdir"]
 template_dir = os.path.join(ctx.yaml_dir, template)
 yaml_dir = ctx.yaml_dir
-workspace_id = ctx.workspace_id
+
+if aps.get_api_version() >= aps.ApiVersion("1.2.0"):
+    workspace_id_kwarg = {"workspace_id": ctx.workspace_id}
+else:
+    workspace_id_kwarg = {}
 
 settings = aps.SharedSettings(ctx.workspace_id, "AnchorpointTemplateSettings")
 template_dir = os.path.join(settings.get("template_dir", template_dir), template_subdir)
@@ -252,7 +256,7 @@ def create_project_from_template(template_path, target_folder, ctx):
     # Create the actual project and write it in the database
     project = ctx.create_project(target, strip_spaces(project_display_name))
     # Copy the whole folder structure and resolve all tokens using the variables dict
-    aps.copy_from_template(source, target, variables, workspace_id=workspace_id)
+    aps.copy_from_template(source, target, variables, **workspace_id_kwarg)
 
     # Add the resolved tokens as metadata to the project
     # This metadata can be used for any file and subfolder templates
@@ -272,11 +276,11 @@ def create_documents_from_template(template_path, target_folder, ctx):
     # Copy the whole folder structure and resolve all tokens using the variables dict
     try:
         if file_mode:
-            aps.copy_file_from_template(template_path, target_folder, variables, workspace_id=workspace_id)
+            aps.copy_file_from_template(template_path, target_folder, variables, **workspace_id_kwarg)
             if callbacks and "file_from_template_created" in dir(callbacks):
                 callbacks.file_from_template_created(target_folder, template_path, variables)
         else:
-            aps.copy_from_template(template_path, target_folder, variables, workspace_id=workspace_id)
+            aps.copy_from_template(template_path, target_folder, variables, **workspace_id_kwarg)
             if callbacks and "folder_from_template_created" in dir(callbacks):
                 callbacks.folder_from_template_created(target_folder, template_path, variables)
 
