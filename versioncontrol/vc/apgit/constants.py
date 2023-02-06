19b25efd0c93f60f842d8915137c1e95cf4d85bb
Jochen Hunz <j.hunz@anchorpoint.app>
2023-02-06 11:27:11 +0100

    force update git on mac to fix certificate

--- versioncontrol/vc/apgit/constants.py
+++ versioncontrol/vc/apgit/constants.py
@@ -1,4 +1,4 @@
 INSTALL_URL_WIN = "https://s3.eu-central-1.amazonaws.com/releases.anchorpoint.app/git/win.zip"
 INSTALL_URL_MAC = "https://s3.eu-central-1.amazonaws.com/releases.anchorpoint.app/git/mac.zip"
 GIT_VERSION_WIN = "git version 2.35.3.windows.1"
-GIT_VERSION_MAC = "git version 2.35.3"
+GIT_VERSION_MAC = "git version 2.35.6"
