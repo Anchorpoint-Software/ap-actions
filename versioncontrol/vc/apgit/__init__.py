429b41cc2f992b3b6a67b4d5e8b64560cec64703
Jochen Hunz <j.hunz@anchorpoint.app>
2023-02-24 16:35:12 +0100

    enable tracer with environment varialbe

--- versioncontrol/vc/apgit/__init__.py
+++ versioncontrol/vc/apgit/__init__.py
@@ -5,7 +5,14 @@ try:
     import vc.apgit_utility.install_git as install_git
     import os
     os.environ["GIT_PYTHON_GIT_EXECUTABLE"] = install_git.get_git_cmd_path().replace("\\","/")
-
+    
+    # User can enable tracing by setting environment variable (set GIT_PYTHON_TRACE=full)
+    # os.environ["GIT_PYTHON_TRACE"] = "full"
+    if "GIT_PYTHON_TRACE" in os.environ:
+        import logging
+        logging.basicConfig()
+        logging.root.setLevel(logging.INFO)
+        
     if utility.guarantee_git():
         try: 
             import git
