fd6efe9705c48209650a26adb99f1f16e27e5131
Jochen Hunz <j.hunz@anchorpoint.app>
2023-03-15 19:50:54 +0100

    Do not use get_locking_application as it is super slow on Windows

--- versioncontrol/actions/git_errors.py
+++ versioncontrol/actions/git_errors.py
@@ -48,23 +48,27 @@ def handle_error(e: Exception):
         print(message)
         file = _get_file_from_error(message)
         application = None
-        if file:
-            application = utility.get_locking_application(file)
+        
+        # This is too slow on Windows, unfortunately
+        # if file:
+        #     application = utility.get_locking_application(file)
 
         d = ap.Dialog()
         d.title = "Git: Could not Change Files"
         d.icon = ":/icons/versioncontrol.svg"
 
-        if application:
-            user_error = f"The file <b>{file}</b><br> could not be removed or changed becausethey are opened by the application <b>{application})</b>.<br>Please close the {application} and try again."
+        if not file:
+            user_error = f"Some file could not be changed because it is opened by an application, or<br>you don't have permissions to write the file."
+        elif application:
+            user_error = f"The file <b>{file}</b><br> could not be changed because it is opened by the application <b>{application}</b>.<br>Please close {application} and try again."
         else:
-            user_error = f"The file <b>{file}</b><br> could not be removed or changed because you don't have permissions to write the file.<br>Please fix the permissions and try again."
+            user_error = f"The file <b>{file}</b><br> could not be changed because it is opened by an application, or<br>you don't have permissions to write the file."
 
         d.add_text(user_error)
         if platform.system() == "Darwin":
-            d.add_info("See more details in the Python console <b>(CMD+SHIFT+P)</b>")
+            d.add_info("Please close the application or fix the permissions and try again.<br>See more details in the Python console <b>(CMD+SHIFT+P)</b>")
         else:
-            d.add_info("See more details in the Python console <b>(CTRL+SHIFT+P)</b>")
+            d.add_info("Please close the application or fix the permissions and try again.<br>See more details in the Python console <b>(CTRL+SHIFT+P)</b>")
 
         d.add_button("OK", callback=lambda d: d.close())
         d.show()
