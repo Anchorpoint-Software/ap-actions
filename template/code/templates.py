9f273c4022b876350e3360095f585eb1fc754cdc
Jochen Hunz <j.hunz@anchorpoint.app>
2023-09-06 10:13:01 +0200

    fix event callback loading

--- template/code/templates.py
+++ template/code/templates.py
@@ -43,9 +43,9 @@ template_dir = os.path.join(ctx.yaml_dir, template)
 yaml_dir = ctx.yaml_dir
 
 settings = aps.SharedSettings(ctx.workspace_id, "AnchorpointTemplateSettings")
-template_dir = os.path.join(get_workspace_template_dir(settings, template_dir), template_subdir)
-callback_file = get_callback_location(settings, template_dir)
-print(callback_file)
+template_root_dir = get_workspace_template_dir(settings, template_dir)
+template_dir = os.path.join(template_root_dir, template_subdir)
+callback_file = get_callback_location(settings, template_root_dir)
 
 if project:
     project_templates_location = template_utility.get_template_dir(project.path)
