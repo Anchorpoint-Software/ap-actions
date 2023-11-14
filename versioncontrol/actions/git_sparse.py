2bf2629aa0c676aaaf13ba15990fd01df0285e65
Matthäus Niedoba <m.niedoba@anchorpoint.app>
2023-11-14 13:54:51 +0100

    Adjusted copy

--- versioncontrol/actions/git_sparse.py
+++ versioncontrol/actions/git_sparse.py
@@ -106,7 +106,7 @@ def on_download_remote_folder(relative_folder_path: str, ctx):
         sys.path.insert(0, script_dir)
         from vc.apgit.repository import GitRepository
 
-        progress = ap.Progress("Downloading folder", show_loading_screen=True, cancelable=False)
+        progress = ap.Progress("Downloading", show_loading_screen=True, cancelable=False)
 
         if ctx.project_path is None:
             raise Exception("project_path is None")
@@ -135,7 +135,7 @@ def on_unload_remote_folder(relative_folder_path: str, ctx):
         sys.path.insert(0, script_dir)
         from vc.apgit.repository import GitRepository
 
-        progress = ap.Progress("Unloading folder", show_loading_screen=True, cancelable=False)
+        progress = ap.Progress("Unloading", show_loading_screen=True, cancelable=False)
 
         if ctx.project_path is None:
             raise Exception("project_path is None")
