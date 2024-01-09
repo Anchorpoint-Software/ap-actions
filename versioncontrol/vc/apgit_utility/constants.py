8f46ad488188ff0c208cd8d9b8a86a9b0f01b753
Jochen Hunz <j.hunz@anchorpoint.app>
2024-01-09 12:57:07 +0100

    set version of git for windows

--- versioncontrol/vc/apgit_utility/constants.py
+++ versioncontrol/vc/apgit_utility/constants.py
@@ -1,4 +1,4 @@
 INSTALL_URL_WIN = "https://s3.eu-central-1.amazonaws.com/releases.anchorpoint.app/git/win.zip"
 INSTALL_URL_MAC = "https://s3.eu-central-1.amazonaws.com/releases.anchorpoint.app/git/mac.zip"
-GIT_VERSION_WIN = "git version 2.39.2.windows.1"
+GIT_VERSION_WIN = "git version 2.39.2.windows.1 ap_1-git-lfs/3.4.1 (GitHub; windows amd64; go 1.20.3; git 995b54fb)"
 GIT_VERSION_MAC = "git version 2.39.2 ap_1-git-lfs/3.4.1 (GitHub; darwin arm64; go 1.21.5; git 995b54fb)"
