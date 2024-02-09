0f1a07656e8bfdea71388177d069976f16fbf4ea
Dennis Schlösser <d.schloesser@anchorpoint.app>
2024-02-09 13:45:35 +0100

    adjusted error message on connection aborted in azure devops integration oauth

--- integrations/azure_devops/azure_devops_integration.py
+++ integrations/azure_devops/azure_devops_integration.py
@@ -308,6 +308,8 @@ class DevopsIntegration(ap.ApIntegration):
         except Exception as e:
             if "No organizations found" in str(e):
                 ap.UI().show_error(title='Azure DevOps authentication failed', duration=6000, description=f'No organizations found. Please visit our <a href="https://docs.anchorpoint.app/docs/1-overview/integrations/azure-devops/#troubleshooting">troubleshooting</a> page to learn how to fix this.')
+            elif "Connection aborted" in str(e):
+                ap.UI().show_error(title='Azure DevOps authentication failed', duration=6000, description=f'The authentication failed, because the connection was aborted. Please try again or visit our <a href="https://docs.anchorpoint.app/docs/1-overview/integrations/azure-devops/#troubleshooting">troubleshooting</a> page.')
             else:
                 ap.UI().show_error(title='Azure DevOps authentication failed', duration=6000, description=f'The authentication failed, because "{str(e)}". Please visit our <a href="https://docs.anchorpoint.app/docs/1-overview/integrations/azure-devops/#troubleshooting">troubleshooting</a> page to learn how to fix this.')
             return
