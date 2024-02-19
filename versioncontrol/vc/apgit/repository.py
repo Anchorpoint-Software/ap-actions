bcd8833e151a0b221db112a0e260549070f8ba35
Jochen Hunz <j.hunz@anchorpoint.app>
2024-02-19 18:01:23 +0100

    fix hard coded use of "origin" remote for set url

--- versioncontrol/vc/apgit/repository.py
+++ versioncontrol/vc/apgit/repository.py
@@ -736,7 +736,10 @@ class GitRepository(VCRepository):
             else:
                 raise Exception("Cannot change remote URL because there are pending changes")
         else:
-            self.repo.git.remote("set-url", "origin", url)
+            branch = self._get_current_branch()
+            remote = self._get_default_remote(branch)
+            remote_name = remote if remote else "origin"
+            self.repo.git.remote("set-url", remote_name, url)
 
             # Make a prune fetch to remove outdated refs from the old remote
             self.repo.git.fetch("--all", "-p")
